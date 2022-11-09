c2e9a42e6a564bf7c580cb5f71f465e43b51fd95
Christopher Adatan <cadatan@impactcompanies.com>
2022-11-09 15:58:06 -0800

    Resize window

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const path = require('path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
