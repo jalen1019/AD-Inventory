341187abb4b0bb5d55e80f14567c66610cbb2af5
Christopher Adatan <cadatan@impactcompanies.com>
2022-11-16 11:48:45 -0800

    Add IndexedDB

--- preload.js
+++ preload.js
@@ -1,7 +1,12 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
 // All the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+const { contextBridge, ipcRenderer } = require('electron')
+// Expose Computer API 
+contextBridge.exposeInMainWorld('computer', {
+
+})
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
@@ -12,11 +17,33 @@ window.addEventListener('DOMContentLoaded', () => {
       replaceText(`${dependency}-version`, process.versions[dependency])
     }
 
-});
+    // Populate the table
+    const request = window.indexedDB.open("ComputerDatabase", 1);
+    let db;
+    
+    request.onerror = (event) => {
+      console.error(event);
+    };
 
-contextBridge.exposeInMainWorld('electron', {
-  store: {
-    set: (key, value) => ipcRenderer.invoke('electron-store-set', key, value),
-    get: (key) => ipcRenderer.invoke('electron-store-get', key),
-  }
-})
+    request.onupgradeneeded = (event) => {
+      db = event.target.result;
+
+      const objectStore = db.createObjectStore("computers", { autoIncrement : true });
+
+      // Add an index for each field present in the database.
+      objectStore.createIndex("previousUser", "previousUser", { unique : false });
+      objectStore.createIndex("currentUser", "currentUser", { unique : false });
+      objectStore.createIndex("computerName", "computerName", { unique : true });
+      objectStore.createIndex("modelName", "modelName", { unique : false });
+      objectStore.createIndex("cpuModel", "cpuModel", { unique : false });
+      objectStore.createIndex("ramSize", "ramSize", { unique : false });
+      objectStore.createIndex("storage", "storage", { unique : false });
+      objectStore.createIndex("storageType", "storageType", { unique : false });
+      objectStore.createIndex("monitorCount", "monitorCount", { unique : false });
+      objectStore.createIndex("monitorSize", "monitorSize", { unique : false });
+
+      request.onsuccess = (event) => {
+        db = event.target?.result;
+      };
+    };
+});
--- renderer.js
+++ renderer.js
@@ -1,16 +1,14 @@
-let store = window.electron.store.getAll();
-
 // Disable form submissions if there are invalid fields
 (function () {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
   
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
+        form.addEventListener('click', function (event) {
           if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
@@ -19,4 +17,36 @@ let store = window.electron.store.getAll();
           form.classList.add('was-validated')
         }, false)
       })
-  })()
+})();
+
+document.getElementById('submitBtn').addEventListener('click', () => {
+    const request = indexedDB.open("ComputerDatabase", 1);
+    let db;
+
+    request.onsuccess = (event) => {
+        db = event.target.result;
+
+        let computer = {
+            previousUser: document.querySelector('#previousUser').value,
+            currentUser: document.querySelector('#currentUser').value,
+            computerName: document.querySelector('#computerName').value,
+            modelName: document.querySelector('#modelName').value,
+            cpuModel: document.querySelector('#cpuModel').value,
+            ramSize: document.querySelector('#ramSize').value,
+            storage: document.querySelector('#storage').value,
+            monitorCount: document.querySelector('#monitorCount').value,
+            //monitorSize: document.querySelector('#monitorSize').value
+        }
+
+        //computer.monitorSize = document.querySelector('#monitorSize').value;
+        
+        let storageTypeValue = document.querySelector('#storageType')
+            .options[storageType.selectedIndex].text;
+        computer.storageType = storageTypeValue;
+        
+        const transaction = db.transaction(["computers"], "readwrite");
+        const computerObjectStore = transaction.objectStore("computers");
+    
+        computerObjectStore.add(computer);
+    };
+});
