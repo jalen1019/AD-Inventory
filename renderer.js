e72b70407111846c1b59b6f1ed90615226de4303
Christopher Adatan <cadatan@impactcompanies.com>
2022-11-21 09:36:13 -0800

    Add edit-button modal-dialog population

--- renderer.js
+++ renderer.js
@@ -1,3 +1,129 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Set up database access
+    const request = window.indexedDB.open("ComputerDatabase", 1);
+    let db;
+      
+    request.onerror = (event) => {
+      console.error(event);
+    };
+  
+    request.onupgradeneeded = (event) => {
+      db = event.target.result;
+  
+      const objectStore = db.createObjectStore("computers", { autoIncrement : true });
+  
+      // Add an index for each field present in the database.
+      objectStore.createIndex("previousUser", "previousUser", { unique : false });
+      objectStore.createIndex("currentUser", "currentUser", { unique : false });
+      objectStore.createIndex("computerName", "computerName", { unique : true });
+      objectStore.createIndex("modelName", "modelName", { unique : false });
+      objectStore.createIndex("cpuModel", "cpuModel", { unique : false });
+      objectStore.createIndex("ramSize", "ramSize", { unique : false });
+      objectStore.createIndex("storage", "storage", { unique : false });
+      objectStore.createIndex("storageType", "storageType", { unique : false });
+      objectStore.createIndex("monitorCount", "monitorCount", { unique : false });
+      objectStore.createIndex("monitorSize", "monitorSize", { unique : false });
+  
+      // On successful database creation/update
+      request.onsuccess = (event) => {
+        db = event.target?.result;
+      };
+    };
+  
+    // Event handler for successful database open
+    request.onsuccess = (event) => {
+      db = event.target?.result;
+      const transaction = db.transaction("computers", "readonly");
+      const computerObjectStore = transaction.objectStore("computers");
+      let request = computerObjectStore.getAll();
+  
+      // On successful database read
+      request.onsuccess = (event) => {
+        let tableBody = document.querySelector('#table-body');
+  
+        // Populate table with information
+        request.result.forEach(element => {
+          // Set up table row and cells for population
+          let row = tableBody.insertRow();
+          let editGroupCell = row.insertCell();
+          let previousUser = row.insertCell();
+          let currentUser = row.insertCell();
+          let computerName = row.insertCell();
+          let modelName = row.insertCell();
+          let cpuModel = row.insertCell();
+          let ramSize = row.insertCell();
+          let storage = row.insertCell();
+          let storageType = row.insertCell();
+          let monitorCount = row.insertCell();
+  
+          // Populate table with information
+          previousUser.innerHTML = element.previousUser;
+          currentUser.innerHTML = element.currentUser;
+          computerName.innerHTML = element.computerName;
+          modelName.innerHTML = element.modelName;
+          cpuModel.innerHTML = element.cpuModel;
+          ramSize.innerHTML = element.ramSize;
+          storage.innerHTML = element.storage;
+          storageType.innerHTML = element.storageType;
+          monitorCount.innerHTML = element.monitorCount;
+  
+          // Create edit and delete buttons
+          let editButtonGroup = document.createElement('div');
+          let editButton = document.createElement('button');
+          let deleteButton = document.createElement('button');
+  
+          // Setup edit and delete button container
+          editButtonGroup.className = 'btn-group';
+          editButtonGroup.setAttribute('role', 'group');
+          
+          // Setup edit button
+          editButton.type = 'button';
+          editButton.classList.add('btn', 'btn-primary', 'editButton');
+          editButton.innerHTML = '✏️';
+          editButton.setAttribute('data-bs-toggle', 'modal');
+          editButton.setAttribute('data-bs-target', '#addRecord');
+          editButton.setAttribute('data-editDeviceModalLabel', 'Edit Device');
+
+          // Add event listener for each edit button which populates the modal with
+          // current row data.
+          editButton.addEventListener('click', event => {
+            let form = document.querySelector('#addDeviceForm');
+            form.querySelector('#previousUser').value = element.previousUser;
+            form.querySelector('#currentUser').value = element.currentUser;
+            form.querySelector('#computerName').value = element.computerName;
+            form.querySelector('#modelName').value = element.modelName;
+            form.querySelector('#cpuModel').value = element.cpuModel;
+            form.querySelector('#ramSize').value = element.ramSize;
+            form.querySelector('#storage').value = element.storage;
+            form.querySelector('#storageType').value = element.storageType;
+            form.querySelector('#monitorCount').value = element.monitorCount;
+          });
+          
+          // Setup delete button
+          deleteButton.type = 'button';
+          deleteButton.classList.add('btn', 'btn-primary', 'deleteButton');
+          deleteButton.innerHTML = '❌';
+  
+          // Delete button modal setup
+          deleteButton.setAttribute('data-bs-toggle', 'modal');
+          deleteButton.setAttribute('data-bs-target', '#deleteModal');
+          
+          // Add edit button group to table cell
+          editButtonGroup.appendChild(editButton);
+          editButtonGroup.appendChild(deleteButton);
+          editGroupCell.appendChild(editButtonGroup);
+        });
+        
+        //*
+        // Define emit custom event to signal completed table population.
+        const customEvent = new Event('tablePopulated');
+        //customEvent.target = tableBody;
+        window.dispatchEvent(customEvent);
+        //*/
+      };
+    };
+  });
+
 // Disable form submissions if there are invalid fields
 (function () {
     'use strict'
@@ -50,21 +176,12 @@ document.getElementById('submitBtn').addEventListener('click', () => {
         const computerObjectStore = transaction.objectStore("computers");
     
         computerObjectStore.add(computer);
+        let modal = document.querySelector('#addRecord');
+        modal.querySelectorAll('input').forEach(element => {
+          element.value = '';
+        });
         location.reload();
     };
-});
-
-// Event listener for edit button click
-window.addEventListener('tablePopulated', event => {
-    let tableBody = event.target;
-    console.log('tableBody: ', tableBody);
-    document.querySelectorAll('.editButton').forEach(element => {
-        element.addEventListener('click', event => {
-            // TODO: On edit button click, convert all row cells to input fields.
-            let row = event.target.closest('tr');
-            console.log(row);
-        });
-    })
 });
 
 // Event handler for delete button click
@@ -75,6 +192,7 @@ deleteModal.addEventListener('shown.bs.modal', (event) => {
     // row.children[3] corresponds to computerName field
     let computerName = row.children[3].innerHTML;
     
+    // Event handler for submission button click.
     document.getElementById('deleteItemModalSubmit').addEventListener('click', (event) => {    
         // Setup database transaction to find item.
         const request = indexedDB.open("ComputerDatabase", 1);
@@ -105,4 +223,10 @@ deleteModal.addEventListener('shown.bs.modal', (event) => {
         };
         location.reload();
     });
+});
+
+// Event handler for hiding modal dialog
+let addRecordModal = document.querySelector('#addRecord');
+addRecordModal.addEventListener('hidden.bs.modal', () => {
+  addRecordModal.querySelector('form').reset();
 });
